8722e2f0db1f32fa973b8a2a3c79c900d3a20acb
tilleyarthur <86635878+tilleyarthur@users.noreply.github.com>
2021-09-05 17:17:46 -0400

    js layout

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var forecastContainerEl=document.querySelector("#forecast-container");
+var citySearchInputEl=document.querySelector("#searched-city");
+var forecastSection=document.querySelector("#forecast");
+var futureContainerEl=document.querySelector("future-container");
+var recentlySearchedButtonEl=document.querySelector("recently-searched");
+
+// set up api key
+
+// var formSubmitHandler preventDefault
+
+// var saveSearch
+
+// var getCityForecast (fetch)
+
+// var displayForecast
+
+// date element
+
+// span elements to hold temp, humidity, wind, uv data (separate fetch api?)
+
+// appendChildren
+
+// var lat and lon
+
+// fetch 5 day forecast
+
+// 
